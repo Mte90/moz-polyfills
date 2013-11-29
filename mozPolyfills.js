9666555d9d26e7d466ebbeac3ffb4367f2b527e0
Mte90 <mte90smanettator@gmail.com>
2013-11-29 17:47:10 +0100

    added check for mime type

--- mozPolyfills.js
+++ mozPolyfills.js
@@ -1,54 +1,53 @@
-if(!navigator['mozSetMessageHandler']){
-    navigator['mozSetMessageHandler'] = function(activity, callback){
-        if(activity === 'open'){
-            // TODO
-            return false;
-        }
-    };
+if (!navigator['mozSetMessageHandler']) {
+	navigator['mozSetMessageHandler'] = function(activity, callback) {
+		if (activity === 'open') {
+			// TODO
+			return false;
+		}
+	};
 }
 
-try {
-    new MozActivity({ });
-} catch(e){
-    
-    /**
-     * @constructor
-     */
-    window['MozActivity'] = function(config){
-        if(config.name === 'pick'){
-            input = document.createElement('input');
-            input.type = 'file';
-            if(config.data && config.data.type){
-                input.accept = config.data.type.join();
-            }
-            input.addEventListener('change', function(e){
-                files = e.target.files; // FileList object
-                
-                if(files.length === 0){
-                    return;
-                }
-                
-                // TODO: Check file types
-                /*
-                if(file.type !== '' && !file.type.match('image.*')){
-                    return;
-                }
-                */
-                
-                this.result = {
-                	blob: files[0]
-                };
-                
-                if(this.onsuccess){
-                    this.onsuccess();
-                }
-                
-            }.bind(this), false);
-            
-            input.click();
-        }
-    };
-    window['MozActivity'].prototype = {
-        constructor: window['MozActivity']
-    };
-}
+/**
+ * Load fake WebActivity
+ * @constructor
+ * @param {config} the config for the webactivity
+ */
+
+window['MozActivity'] = function(config) {
+	//pick image
+	if (config.name === 'pick') {
+		input = document.createElement('input');
+		input.type = 'file';
+		if (config.data && config.data.type) {
+			input.accept = config.data.type.join();
+		}
+		input.addEventListener('change', function(e) {
+			files = e.target.files; // FileList object
+
+			if (files.length === 0) {
+				return;
+			}
+			
+			//check mimetype
+			 if(files[0].type !== '' && config.data.type.indexOf(files[0].type) < 0){
+				return;
+			 }
+			 
+
+			this.result = {
+				blob: files[0]
+			};
+
+			if (this.onsuccess) {
+				this.onsuccess();
+			}
+
+		}.bind(this), false);
+
+		input.click();
+	}
+};
+window['MozActivity'].prototype = {
+	constructor: window['MozActivity']
+};
+
